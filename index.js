265c6033385b4aa4b04fbaade1381d285193aab4
hossainchisty <hossain.chisty11@gmail.com>
2023-03-28 16:49:38 +0600

    ✨ Refactor ```index.js```

--- index.js
+++ index.js
@@ -1,50 +1,36 @@
 const express = require("express");
-const port = 3000;
 const dotenv = require("dotenv").config();
 // Database connection with mongoose
-const connectDB = require("./src/infrastructure/dataSources/database");
+const connectDB = require("./config/db");
 connectDB();
 // Set up middleware, controllers, and routes
-const { errorHandler } = require("./src/application/middlewares/errorMiddleware");
+const { errorHandler } = require("./middlewares/errorMiddleware");
+
+// Server Port
+const port = process.env.PORT || 5000;
+
+// Routers
+const chefRouters = require('./routers/chefRouters');
+const campaignRouters = require('./routers/marketing/campaignRouters');
+const categoryRouters = require('./routers/categoryRouters');
+const reservationRouters = require('./routers/reservationRouters');
 
 // Express app initialization
 const app = express();
 app.use(express.json());
 app.use(errorHandler);
-// app.use('/users', userRoutes);
 
-app.get("", (req, res) => {
-  res.send({
-    message: "Oky bad boy!",
-  });
-});
+
+// Application routes
+app.use("/api/v1/chef/", chefRouters);
+app.use("/api/v1/our/campaign/", campaignRouters);
+app.use("/api/v1/category/", categoryRouters);
+app.use("/api/v1/reservation/", reservationRouters);
 
 // Undefined Route Implement
-app.use("*",(req,res)=>{
-  res.status(404).json({status:"fail",data:"Not Found"})
-})
-
-app.use("/api/v1/", require("./src/application/routers/routers"));
-
-// Practice Mongodb database
-// run();
-// async function run() {
-//   try {
-//     const reservation = await Reservation.create({
-//       full_name: "Reza",
-//       email: "Reza@gmail.com",
-//       phone_number: "+8801537377306",
-//       date: 3 / 14 / 2023,
-//       time: "8.00PM",
-//       branch: 'Baddubai',
-//     });
-//     console.log(reservation);
-//     // const data = await Reservation.find({});
-//     // console.log(data);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
+app.use("*", (req, res) => {
+  res.status(404).json({ status: "Fail", data: "Not Found" });
+});
 
 // Start the server
 app.listen(port, () => {
